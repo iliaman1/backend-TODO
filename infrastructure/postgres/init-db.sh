225b08c3b9c318ca6223835fa7938879782ca618
ilia <iliaforwork1993@gmail.com>
2026-03-13 17:56:59 +0300

    refactor: Consolidate databases into a single PostgreSQL service

--- infrastructure/postgres/init-db.sh
+++ infrastructure/postgres/init-db.sh
@@ -2,5 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    ALTER USER "$POSTGRES_USER" WITH CREATEDB;
+    CREATE DATABASE auth_db;
+    CREATE DATABASE todo_db;
+    CREATE DATABASE files_db;
 EOSQL
